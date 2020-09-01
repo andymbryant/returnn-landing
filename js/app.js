bed9cc87a606719edc3969037a1fba1f694f4310
Andy Bryant <a.mcdermid.bryant@gmail.com>
2020-08-31 22:36:15 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const party = new Date("October 10, 2020 05:00:00").getTime();
+function getTime() {
+  const today = new Date().getTime();
+
+  // get the difference
+  const diff = party - today;
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // display
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+  setTimeout(getTime, 1000)
+}
+
+getTime()
